ba4af73e33fec98b51c37190abb3f3ec037a12ce
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-06 23:39:06 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,44 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailParser
+  
+  attr_accessor :emails
+
+  def initialize(emails) 
+    @emails = emails
+  end
+  
+  def parse 
+    #address = @emails.split('', ',').uniq!
+    address = @emailS.split(/[,][\s]/).uniq!
+  end
+end
+   
+   
+  # def self.genres 
+  #   @@genres.uniq! 
+  # end
+  
+  
+#   describe "EmailAddressParser" do
+#   describe '#parse' do
+#     it "parses CSV emails" do
+#       expect(EmailAddressParser.new("avi@test.com, arel@test.com").parse).to eq(["avi@test.com", "arel@test.com"])
+#     end
+
+#     it "parses space delimited emails" do
+#       expect(EmailAddressParser.new("avi@test.com arel@test.com").parse).to eq(["avi@test.com", "arel@test.com"])
+#     end
+
+#     it "parses both CSV and space delimited emails" do
+#       emails = "avi@test.com, arel@test.com test@avi.com, test@arel.com"
+#       expect(EmailAddressParser.new(emails).parse).to eq(["avi@test.com", "arel@test.com","test@avi.com", "test@arel.com"]) 
+#     end
+
+#     it 'parses and removes duplicate emails' do
+#       expect(EmailAddressParser.new("avi@test.com, avi@test.com").parse).to eq(["avi@test.com"])
+#     end
+#   end
+# end
